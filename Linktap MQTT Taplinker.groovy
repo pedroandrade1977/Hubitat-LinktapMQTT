1edffff02be62534a0c4bbad4b7a4b287501e45f
pedroandrade1977 <59777383+pedroandrade1977@users.noreply.github.com>
2022-01-23 16:49:07 +0200

    Update Linktap MQTT Taplinker.groovy

--- Linktap MQTT Taplinker.groovy
+++ Linktap MQTT Taplinker.groovy
@@ -16,7 +16,8 @@
  *
  *	Updates:
  *	Date: 2022-01-10	v1.0 Initial release
- *  	Date: 2022-01-18    	v1.1 MQTT reconnection functionality, daily/weekly/monthly/yearly water volumes
+ *  Date: 2022-01-18    v1.1 MQTT reconnection functionality, daily/weekly/monthly/yearly water volumes
+ *  Date: 2022-01-23    v1.2 Fixed issue with water volumes, added command for submission of rain data to gateway, changed child device logging
 */
 
 import java.text.DecimalFormat
@@ -45,10 +46,12 @@ metadata {
         attribute "manualMode","boolean"
         attribute "planMode","number"
         attribute "childLock","string"
-        attribute "dailyVolume","number"
-        attribute "weeklyVolume","number"
-        attribute "monthlyVolume","number"
-        attribute "yearlyVolume","number"
+/*1.1*/ attribute "dailyVolume","number"
+/*1.1*/ attribute "weeklyVolume","number"
+/*1.1*/ attribute "monthlyVolume","number"
+/*1.1*/ attribute "yearlyVolume","number"
+/*1.2*/ attribute "rainSkipNotification","string"         // timestamp of the latest skipped watering
+
         
         // start watering with parametric duration
         command "timedStart", [[name: "Duration*", type: "NUMBER", description: "Watering session duration in seconds (for G1/G2 must be a multiple of 60, for G1s/G2s any number between 3 and 86399)", constraints: ["NUMBER"]]]
@@ -76,6 +79,10 @@ metadata {
         
         // execute and print out to log wireless test between device and gateway
         command "testWireless"
+
+        // update current values in hubitat
+/*1.2*/ command "setVolumes", [[name: "daily", type: "NUMBER"], [name: "weekly", type: "NUMBER"], [name: "monthly", type: "NUMBER"], , [name: "yearly", type: "NUMBER"]]
+    
     }
     
     preferences {
@@ -90,72 +97,71 @@ metadata {
 
 
 def close(){
-    (debug=="yes")?log.debug("Close()"):null;
+    parent.logDebug("Close()");
     parent.sendCommand("stop water",[device.currentValue("gatewayId") as String, device.getDeviceNetworkId().substring(prefixLength)]);     
 }
 
 def open(){
     Integer param=autoClose as Integer;
-    (debug=="yes")?log.debug("Open() command with autoclose: ${param}"):null;
+    parent.logDebug("Open() command with autoclose: ${param}");
     parent.sendCommand("start water",[device.currentValue("gatewayId") as String, device.getDeviceNetworkId().substring(prefixLength), param]);
 }
 
 def timedStart(duration) {
-    (debug=="yes")?log.debug("timedStart() command with duration: ${duration}"):null;
+    parent.logDebug("timedStart() command with duration: ${duration}");
     parent.sendCommand("start water",[device.currentValue("gatewayId") as String, device.getDeviceNetworkId().substring(prefixLength), duration as Integer]);
 }
 
 def dismissAlert(alertType) {
-    (debug=="yes")?log.debug("dismissAlert()"):null;
+    parent.logDebug("dismissAlert()");
     parent.sendCommand("dismiss alert", [device.currentValue("gatewayId") as String, device.getDeviceNetworkId().substring(prefixLength), alertType]);
 }
 
 def instantWateringPlan(timestamp, duration) {
     Random r=new Random();
-    (debug=="yes")?log.debug("instantWateringPlan with timestamp $timestamp and duration $duration}"):null;
+    parent.logDebug("instantWateringPlan with timestamp $timestamp and duration $duration}");
     parent.sendCommand("set plan", [device.currentValue("gatewayId") as String, device.getDeviceNetworkId().substring(prefixLength), r.nextInt(10000), 1, 0,0, timestamp, duration as Integer]);
 }
 
 def deleteWateringPlan() {
-    (debug=="yes")?log.debug("deleteWateringPlan()"):null;
+    parent.logDebug("deleteWateringPlan()");
     parent.sendCommand("delete plan", [device.currentValue("gatewayId") as String, device.getDeviceNetworkId().substring(prefixLength)]);
 }
 
 def childLock(lockType) {
-    (debug=="yes")?log.debug("childLock() with input $lockType"):null;
+    parent.logDebug("childLock() with input $lockType");
     parent.sendCommand("child lock", [device.currentValue("gatewayId") as String, device.getDeviceNetworkId().substring(prefixLength), lockType]);
 }
 
 def enableAlert(alertType) {
-    (debug=="yes")?log.debug("enableAlert() with input $alertType"):null;
+    parent.logDebug("enableAlert() with input $alertType");
     parent.sendCommand("enable alert", [device.currentValue("gatewayId") as String, device.getDeviceNetworkId().substring(prefixLength), alertType]);
 }
 
 def disableAlert(alertType) {
-    (debug=="yes")?log.debug("disableAlert() with input $alertType"):null;
+    parent.logDebug("disableAlert() with input $alertType");
     parent.sendCommand("disable alert", [device.currentValue("gatewayId") as String, device.getDeviceNetworkId().substring(prefixLength), alertType]);
 }
 
 def fetchGatewayTime() {
-    (debug=="yes")?log.debug("fetchGatewayTime()"):null;
+    parent.logDebug("fetchGatewayTime()");
     parent.sendCommand("fetch time", [device.currentValue("gatewayId") as String]);
 }
 
 def testWireless() {
-    (debug=="yes")?log.debug("testWireless()"):null;
+    parent.logDebug("testWireless()");
     parent.sendCommand("test wireless", [device.currentValue("gatewayId") as String, device.getDeviceNetworkId().substring(prefixLength)]);
 }
 
+/* 1.1 */
 private resetVolumes() {
     Date curDate=new Date();
     Integer day = curDate.date;
     Integer month = curDate[Calendar.DAY_OF_MONTH];
     Integer weekday = curDate[Calendar.DAY_OF_WEEK];
     
-    if (debug=="yes") {
-        log.debug("resetVolumes()");
-        log.debug("day=$day, month=$month, weekday=$weekday");
-    }
+    parent.logDebug("resetVolumes()");
+    parent.logTrace("day=$day, month=$month, weekday=$weekday");
     
     sendEvent(name: "dailyVolume", value: 0);
         
@@ -165,16 +171,31 @@ private resetVolumes() {
     
 }
 
+/* 1.1, updated 1.2 */
 public addVolume(volume) {
-    (debug=="yes")?log.debug("addVolume()"):null;
     
-    sendEvent(name: "dailyVolume", value: (device.currentValue("dailyVolume")?:0)+volume);
-    sendEvent(name: "weeklyVolume", value: (device.currentValue("weeklyVolume")?:0)+volume);
-    sendEvent(name: "monthlyVolume", value: (device.currentValue("monthlyVolume")?:0)+volume);
-    sendEvent(name: "yearlyVolume", value: (device.currentValue("yearlyVolume")?:0)+volume);
+    parent.logDebug("addVolume()");
     
+    daily=(device.currentValue("dailyVolume")?:0)+volume;
+    weekly=(device.currentValue("weeklyVolume")?:0)+volume;
+    monthly=(device.currentValue("monthlyVolume")?:0)+volume;
+    yearly=(device.currentValue("yearlyVolume")?:0)+volume;
+    
+    setVolumes(daily, weekly, monthly, yearly);
+   
 }
 
+/* 1.2 */
+private setVolumes(daily=null, weekly=null, monthly=null, yearly=null) {
+
+    parent.logDebug("setVolumes()");
+    
+    (daily!=null)?sendEvent(name: "dailyVolume", value: daily):null;
+    (weekly!=null)?sendEvent(name: "weeklyVolume", value: weekly):null;
+    (monthly!=null)?sendEvent(name: "monthlyVolume", value: monthly):null;
+    (yearly!=null)?sendEvent(name: "yearlyVolume", value: yearly):null;
+
+}
 
 private updated() {
     schedule("0 0 0 * * ?","resetVolumes");   
